9e8dba3bab259355fda47d033235c7add4f5a1ea
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-09 20:23:13 +0600

    problem solving

---
+++ problem1.js
@@ -0,0 +1,5 @@
+var totalTk=1000;
+var apple=100;
+var orange=200;
+var cost=(apple+orange);
+console.log(totalTk-cost);
---
+++ problem2.js
@@ -0,0 +1,7 @@
+var phy=70;
+var che=90;
+var math=80;
+var bio=78;
+var bangla=90;
+var avg=(phy+che+math+bio+bangla)/5;
+console.log(avg)
