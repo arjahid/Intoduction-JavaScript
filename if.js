af52a80fbdb30c9fae14dfd9a5884af8e4e2428b
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-09 22:17:35 +0600

    if else statement

---
+++ if.js
@@ -0,0 +1,6 @@
+var biriyani=490;
+if(biriyani<400){
+    console.log("Give me one packet")
+}else{
+    console.log("Thak mama lalgbo nh")
+}
---
+++ multiple.js
@@ -0,0 +1,18 @@
+const salary=10000;
+const height=61;
+const bcs=true;
+// if(salary>200000 && height>66 ){
+//     console.log("ok i am done")
+// }else{
+//     console.log("OI mama nh pls")
+// }
+// if(salary>200000 || height>66 || bcs==true ){
+//     console.log("ok i am done")
+// }else{
+//     console.log("OI mama nh pls")
+// }
+if((salary>200000 && height>66) || bcs==true ){
+    console.log("ok i am done")
+}else{
+    console.log("OI mama nh pls")
+}
