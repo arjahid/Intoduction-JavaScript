fb8907313fb99d03372bb8f4e7ea61a8f68d2c51
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-13 16:42:20 +0600

    solve problem

---
+++ Js_loop/prblem11.js
@@ -0,0 +1,12 @@
+
+let sum=0;
+for(let i=1;i<100;i++){
+    sum=sum+i;
+    if(sum>100){
+        break;
+        
+    }
+    console.log(i,sum);
+   
+    
+}
---
+++ Js_loop/problem12.js
@@ -0,0 +1,8 @@
+let square=0;
+for(let i=1;i<100;i++){
+    square=i*i;
+    if(square>100){
+        break;
+    }
+    console.log(square);
+}
