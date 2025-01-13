bb03c2a181846e9bf743ef8d92dcf64168f8f693
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-13 11:47:44 +0600

    starting loop

---
+++ Jsloop/index.js
@@ -0,0 +1,9 @@
+const number=[12,34,54,45,56,45,34];
+console.log(number)
+for(const num of number ){
+    console.log(num)
+}
+const fruits=["mango","banana","apple"]
+for(const fruit of fruits){
+    console.log(fruit)
+}
