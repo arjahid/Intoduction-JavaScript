eabcedf507cad64bf439919fd8d70dd1cf31272e
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-16 14:05:04 +0600

    problem 11

---
+++ string_js/sort.js
@@ -0,0 +1,6 @@
+// const person=['jahid','asha','mony'];
+// const sort=person.sort();
+// console.log(sort)
+const number=[1,2,26,15,7,24,3,2,3];
+const sort=number.sort((a,b)=>{return a-b});
+console.log(sort)
