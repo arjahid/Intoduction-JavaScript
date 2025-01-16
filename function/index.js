0d08cab74b2473d98d614d5688128ed7eb9ad7ca
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-16 19:41:06 +0600

    function

---
+++ function/index.js
@@ -0,0 +1,7 @@
+const number=45;
+const fanOffKor=()=>{
+    console.log("fan off kor");
+    console.log("light off kor");
+    console.log("Tv off kor");
+}
+fanOffKor();
---
+++ function/index2.js
@@ -0,0 +1,6 @@
+const sum=(x,y)=>{
+    const result=x+y;
+    return result;
+}
+const total=sum(5,5);
+console.log(total)
