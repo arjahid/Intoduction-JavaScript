77a060069b7ca749135fdfb12fc2ac1a597211d7
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-16 13:21:21 +0600

    objecct

---
+++ string_js/object.js
@@ -0,0 +1,10 @@
+const mobile={
+    brand:"apple",
+    model:"i phone 14 pro max",
+    price:120000,
+    color:"blue",
+    isNew:true,
+}
+for(const prop in mobile){
+    console.log(mobile[prop])
+}
