064884bb3037f06480d3b257864d1f1e34f0df3c
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-16 13:45:46 +0600

    array 2

---
+++ string_js/looping.js
@@ -0,0 +1,5 @@
+const friends=['jahid','hasan','ar']
+for(let i=0;i<friends.length;i++){
+    console.log(friends[i])
+
+}
---
+++ string_js/reversea.js
@@ -0,0 +1,4 @@
+const number=[1,2,3,4,5,6,7,8,9,10];
+console.log(number);
+number.reverse();
+console.log(number)
