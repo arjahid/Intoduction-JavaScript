7167a279b57826b1e10ff2ae37c6e2707c38cb96
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-17 16:27:28 +0600

    big number find

---
+++ js_problem/big.js
@@ -0,0 +1,18 @@
+const a=5,b=4,c=7;
+if(a>b && a>c){
+    console.log(" a is big");
+}else if(b>a && b>c){
+    console.log("big is big")
+}else{
+    console.log("c is big")
+}
+// const num=(num1,num2,num3)=>{
+//     if(num1>num2 && num1>num3){
+//         console.log("num 1 is big")
+//     }else if(num2>num1&& num2>num3){
+//         console.log("num 2 is big")
+//     }else{
+//         console.log("num 3 is big")
+//     }
+// }
+// num(1,5,4)
