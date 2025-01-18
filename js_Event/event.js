c6e72f7d79354838253841bd66b466e726227649
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-18 16:23:54 +0600

    javascript event

---
+++ js_Event/event.js
@@ -0,0 +1,7 @@
+const blueButton = ()=>{
+    document.body.style.backgroundColor = "blue";
+}
+const redBtn=document.getElementById("redButton");
+redBtn.onclick = ()=>{
+    document.body.style.backgroundColor = "red";
+}
