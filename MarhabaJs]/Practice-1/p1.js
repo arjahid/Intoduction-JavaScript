664e34b4a9e7952411f5bb19112549f3ebdf39ad
arjahid <jhjahidhasanar@gmail.com>
2025-03-08 16:29:02 +0600

    solve problemset1

---
+++ MarhabaJs]/Practice-1/p1.js
@@ -0,0 +1,15 @@
+const age=23;
+const booktoread=10;
+const wakeuptime=6;
+const wastedTime=120;
+const goal=4;
+const pillow=5;
+console.log(age);
+console.log(booktoread);
+console.log(wakeuptime);
+console.log(wastedTime);
+console.log(goal);
+console.log(pillow);
+
+
+
