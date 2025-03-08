13c9ad4a56995948531ae9b8330db2a514592e99
arjahid <jhjahidhasanar@gmail.com>
2025-03-08 16:34:12 +0600

    solve practice2

---
+++ MarhabaJs]/Practice-1/p2.js
@@ -0,0 +1,19 @@
+const name='jahid';
+const address='Dhaka';
+const institue='DIU';
+const movieOfTheYear='Avengers';
+const hobby='coding';
+const favFood='Biriyani';
+const favColor='Black';
+const favNumber=10;
+const favouritePlayer='Messi';
+console.log(name);
+console.log(address);
+console.log(institue);
+console.log(movieOfTheYear);
+console.log(hobby);
+console.log(favFood);
+console.log(favColor);
+console.log(favNumber);
+console.log(favouritePlayer);
+    
