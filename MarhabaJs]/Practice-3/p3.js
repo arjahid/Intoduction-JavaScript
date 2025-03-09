178f97cabdd8c1b8ae3f7e0280efda85b7f52940
arjahid <jhjahidhasanar@gmail.com>
2025-03-10 02:39:36 +0600

    solve practice-3 problem-2

--- MarhabaJs]/Practice-3/p3.js
+++ MarhabaJs]/Practice-3/p3.js
@@ -1,4 +1,8 @@
 let username='marhaba';
 let userName='jahid';
+let userNames='marhaba jahid';
+let user_name='marhaba jahid';
 console.log(username);
 console.log(userName);
+console.log(userNames);
+console.log(user_name);
